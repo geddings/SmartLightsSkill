8d337ac22a076d197205ea1d1d26169cd176c3fe
Caleb <calinduff@gmail.com>
2017-12-14 14:53:12 -0500

    test count

--- pi/hue_controller.py
+++ pi/hue_controller.py
@@ -32,6 +32,7 @@ class lightArray:
                     b.set_light([1, 2], "on", True, transitiontime=0)
                     time.sleep(1)
                     b.set_light([1, 2], "on", False, transitiontime=0)
+
             elif state['effect'] == "cycle":
                 evenodd = 0
                 b.set_light([1, 2], "on", False, transitiontime=0)
@@ -44,6 +45,18 @@ class lightArray:
                         b.set_light(2, "on", True, transitiontime=0)
                     evenodd = evenodd + 1
                     time.sleep(1)
+
+            elif state['effect'] == "count":
+                evenodd = 0
+                b.set_light([1, 2], "on", False, transitiontime=0)
+                while lightArray.lightcheck == "on" and lightArray.effectcheck != "none":
+                    if evenodd % 2 == 0:
+                        b.set_light(1, "on", False, transitiontime=0)
+                    else:
+                        b.set_light(1, "on", True, transitiontime=0)
+                    time.sleep(1)
+
+
             else:
                 b.set_light([1, 2], "on", True)
         else:
