c665771a8f15364f582b5948bc1af5cab2500097
Caleb <calinduff@gmail.com>
2017-12-15 12:15:47 -0500

    debug brightness

--- pi/hue_controller.py
+++ pi/hue_controller.py
@@ -24,6 +24,7 @@ class lightArray:
         lightArray.lightcheck = state['lights']
         lightArray.effectcheck = state['effect']
         lightArray.brightcheck = state['brightness']
+        print(lightArray.brightcheck)
 
         if state['lights'] == "on":
             if state['effect'] == "blink":
